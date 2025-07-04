ea6c9449ecc194ced0e506f5f59e3374faf9652e
Bora Eren Erdem <erdemeren3460@gmail.com>
2025-07-04 20:12:33 +0300

    User, Lesson and Progress

---
+++ models.py
@@ -0,0 +1,55 @@
+
+
+from sqlalchemy import Column, String, Integer, Float, Boolean, ForeignKey, Table
+from database import Base
+from sqlalchemy.orm import relationship
+
+# bu kısımda ilişki many to many olucak çünkü birden fazla kullanıcı birden fazla ders alabilir. Ayrıca bu tür many to many ilişkilerde direkt ForeignKey ile bağlamak mümkün olmaz. Bu yüzden ara tablo yaptım.   # many-to-many ilişkili ara tablolarda her iki sütun da primary key olarak tanımlanır.
+
+
+user_lessons = Table(
+    'user_lessons',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True, index=True),
+    Column('lesson_id', Integer, ForeignKey('lessons.id'), primary_key=True, index=True)
+)
+
+
+
+class Progress(Base):
+    __tablename__ = 'progress'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    lesson_id = Column(Integer, ForeignKey('lessons.id'), index=True)
+    completed_questions = Column(Integer, default=0)
+    total_questions = Column(Integer, default=0)
+    completion_percentage = Column(Float, default=0.0)
+
+    user = relationship('User', back_populates='progress')
+    lesson = relationship('Lesson', back_populates='progress')
+
+
+class Lesson(Base):
+    __tablename__ = 'lessons'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, unique=True, index=True)
+    description = Column(String, nullable=True)
+    category = Column(String, nullable=True) # web geliştirme, ai vb.
+
+    users = relationship('User', secondary=user_lessons, back_populates='lessons')
+    progress = relationship('Progress', back_populates='lesson')
+
+
+class User(Base): # sorguları hızlandırmak için genel olarak hepsinde index=True diyerek index oluşturdum (böylece select işlemi hızlanır)
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String) # parolanın şifrelenmiş hali
+    role = Column(String, default='user', index=True) # admin ve kullanıcıyı ayırmak için role ekledim
+
+    lessons = relationship('Lesson', secondary=user_lessons, back_populates='users')
+    progress = relationship('Progress', back_populates='user')
