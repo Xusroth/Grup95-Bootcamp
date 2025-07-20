b0d2cd727837a58f5e5bd78fb387f2b613c29c17
Bora Eren Erdem <erdemeren3460@gmail.com>
2025-07-20 19:52:17 +0300

    Guest users are now blocked from creating error reports. A bug in the error report creation section has been fixed. An update error report endpoint has been added for administrators.

--- routers/error.py
+++ routers/error.py
@@ -6,7 +6,7 @@ from typing import Annotated
 from database import SessionLocal
 from starlette import status
 from models import ErrorReport, User
-from schemas import ErrorReportCreate, ErrorReportResponse
+from schemas import ErrorReportCreate, ErrorReportResponse, ErrorReportUpdate
 from routers.auth import get_current_user
 
 router = APIRouter(prefix='/error', tags=['Error Reporting'])
@@ -24,9 +24,12 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.post('/report', status_code=status.HTTP_201_CREATED, response_model=ErrorReportResponse)
 async def report_error(db: db_dependency, report: ErrorReportCreate, current_user: User = Depends(get_current_user)):
+    if not current_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Hata raporu oluşturmak için oturum açmış olmalısınız.")
+
     try:
         db_report = ErrorReport(
-            user_id=report.user_id or (current_user.id if current_user else None),
+            user_id=current_user.id,
             error_message=report.error_message,
             details=report.details
         )
@@ -47,3 +50,26 @@ async def get_error_reports(db: db_dependency, current_user: User = Depends(get_
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Sadece adminler hata raporlarını görebilir.")
     reports = db.query(ErrorReport).all()
     return reports
+
+
+@router.put('/reports/{report_id}', response_model=ErrorReportResponse)
+async def update_error_report(db: db_dependency, report_id: int, report_update: ErrorReportUpdate, current_user: User = Depends(get_current_user)):
+    if current_user.role != 'admin':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Sadece adminler hata raporlarını güncelleyebilir.")
+
+    report = db.query(ErrorReport).filter(ErrorReport.id == report_id).first()
+    if not report:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hata raporu bulunamadı.")
+
+    if report_update.error_message:
+        report.error_message = report_update.error_message
+
+    if report_update.details:
+        report.details = report_update.details
+
+    if report_update.status:
+        report.status = report_update.status
+
+    db.commit()
+    db.refresh(report)
+    return report
