2210a8f5932ac5dac41459ee4b4de0ec20447e4a
Bora Eren Erdem <erdemeren3460@gmail.com>
2025-07-27 01:33:53 +0300

    fix and update

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ load_dotenv() # .env dosyasını yükler
 
 SECRET_KEY = os.getenv('SECRET_KEY') # signature
 ALGORITHM = os.getenv('ALGORITHM', 'HS256') # daha güvenli algoritma kullanabiliriz
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCES_TOKEN_EXPIRE_MINUTES', 30)) # token'ın geçerlilik süresi 30 dakika istersek değiştirebiliriz
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCES_TOKEN_EXPIRE_MINUTES', 30)) # token'ın geçerlilik süresi 10080 dakika (7 gün) istersek değiştirebiliriz
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./codebite.db')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
