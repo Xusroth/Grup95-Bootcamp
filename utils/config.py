e2f1ec4062b151f47ab8965abbebef322c26afc8
Bora Eren Erdem <erdemeren3460@gmail.com>
2025-07-30 17:04:54 +0300

    refresh token and access token updated

--- utils/config.py
+++ utils/config.py
@@ -8,7 +8,8 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY') # signature
 ALGORITHM = os.getenv('ALGORITHM', 'HS256') # güvenlik algoritması
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30)) # token'ın geçerlilik süresi
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30)) # access token geçerlilik süresi
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv('REFRESH_TOKEN_EXPIRE_DAYS'), 30) # refresh token geçerlilik süresi
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587)) # 587 portu güvenli bağlantıyla e posta göndermek için en yaygın kullanılan port
