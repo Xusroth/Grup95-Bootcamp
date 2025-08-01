5d41ce0179cc9b18bfc1455f314dc3fce7a68b86
Bora Eren Erdem <erdemeren3460@gmail.com>
2025-08-01 16:49:26 +0300

    bugs fixed and edited

--- utils/email.py
+++ utils/email.py
@@ -8,7 +8,21 @@ from utils.config import EMAIL_HOST, EMAIL_PORT, EMAIL_ADDRESS, EMAIL_PASSWORD
 
 
 def send_reset_email(email: str, token: str):
-    msg = MIMEText(f'Şifrenizi sıfırlamak için bu linke tıklayın: https://codebite-backend.onrender.com/reset-redirect?token={token}') # bu kısma redirect'tan sonra flutterdaki yolu yazcaz
+    redirect_link = f'https://codebite-backend.onrender.com/auth/reset-redirect?token={token}'
+    deeplink = f'codebite://reset-password?token={token}'
+    plain_content = f"""
+        Merhaba,
+        
+        Şifrenizi sıfırlamak için aşağıdaki bağlantıya tıklayın:
+        {redirect_link}
+        
+        Bağlantı çalışmazsa, yukarıdaki linki kopyalayıp tarayıcınıza veya uygulamanıza yapıştırın.
+        Bu bağlantı 1 saat boyunca geçerlidir. Eğer bu talebi siz yapmadıysanız, lütfen bu e-postayı dikkate almayın.
+        
+        Codebite Ekibi
+    """
+
+    msg = MIMEText(plain_content, 'plain')
     msg['Subject'] = 'Codebite Şifre Sıfırlama'
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = email
@@ -18,5 +32,6 @@ def send_reset_email(email: str, token: str):
             server.starttls()
             server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
             server.sendmail(EMAIL_ADDRESS, email, msg.as_string())
+
     except Exception as err:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"E-posta gönderilemedi. {err}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"E-posta gönderilemedi: {err}")
